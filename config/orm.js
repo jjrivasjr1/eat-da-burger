8af3cc63b4327b843aaef9bf02ac19e8309c8829
jjrivasjr <jjrivasjr1@gmail.com>
2018-03-10 02:12:38 -0600

    commit

--- config/orm.js
+++ config/orm.js
@@ -7,7 +7,25 @@ var orm = {
 			if(err) throw err;
 			cb(result)
 		})
+	},
+
+	update: function(tableInput, condition, cb){
+		connection.query('UPDATE '+tableInput+' SET devoured=ture WHERE id='
+			+condition+';', function(err,result){
+			if(err)throw err;
+			cb(result);
+		})
+	},
+
+	create: function(tableInput,val,cb){
+		connection.query('INSERT INTO '+tableInput+" (burger_name) VALUES ('"+val+"');",
+			function(err,result){
+				if(err)throw err;
+				cb(result);
+			})
 	}
+
+
 }
 
 module.exports = orm;
